917d38eca3a2bb8f26081ae79c7546f5a8e9bd8c
q3dm42 <sergoteq@gmail.com>
2023-05-14 12:48:26 +0300

    HW 2.2 готово до 7 задания

---
+++ script.js
@@ -0,0 +1,45 @@
+// Задание 1
+let a = 10;
+alert(a + 10);
+
+// Задание 2
+let yearOfManufacture = (2007);
+alert("Год выпуска первого iPhone: " + yearOfManufacture);
+
+// Задание 3
+let jsCreator = "Brendan Eich";
+alert(jsCreator)
+
+// Задание 4
+const b = 10;
+const c = 2;
+let sum = b + c;
+let dif = b - c;
+let product = b * c;
+let quotient = b / c;
+
+alert("Сумма: " + sum);
+alert("Разность: " + dif);
+alert("Произведение: " + product);
+alert("Частное: " + quotient);
+
+// Задание 5
+let result = 2 ** 5;
+alert("2 в 5ой степени равно: " + result);
+
+
+// Задание 6
+let d = 9;
+let f = 2;
+let remain = d % f;
+alert("Остаток: " + remain);
+
+// Задание 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
