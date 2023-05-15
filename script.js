0cc014b9d95ba197b454024bae28b3555964b57e
q3dm42 <sergoteq@gmail.com>
2023-05-15 12:48:11 +0300

    Надеюсь готово)

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ alert(a + 10);
 
 // Задание 2
 let yearOfManufacture = (2007);
-alert("Год выпуска первого iPhone: " + yearOfManufacture);
+alert(`Год выпуска первого iPhone: ${yearOfManufacture}`);
 
 // Задание 3
 let jsCreator = "Brendan Eich";
@@ -18,21 +18,21 @@ let dif = b - c;
 let product = b * c;
 let quotient = b / c;
 
-alert("Сумма: " + sum);
-alert("Разность: " + dif);
-alert("Произведение: " + product);
-alert("Частное: " + quotient);
+alert(`Сумма: ${sum}`);
+alert(`Разность: ${dif}`);
+alert(`Произведение: ${product}`);
+alert(`Частное: ${quotient}`);
 
 // Задание 5
 let result = 2 ** 5;
-alert("2 в 5ой степени равно: " + result);
+alert(`2 в 5ой степени равно: ${result}`);
 
 
 // Задание 6
 let d = 9;
 let f = 2;
 let remain = d % f;
-alert("Остаток: " + remain);
+alert(`Остаток: ${remain}`);
 
 // Задание 7
 let num = 1;
@@ -45,17 +45,31 @@ num--;
 alert(num);
 
 // Задание 8
-let age = prompt("Сколько вам лет?");
-alert("Вам " + age + " лет");
+let age = prompt("Cколько вам лет?");
+alert('Вам ' + age + ' лет');
 
-// Задание 9
+// Задание 9.0
 let user = {
     name: "Сергей",
     age: 34,
     isAdmin: true
 };
 
+// Задание 9.1
+user.cityOfResidence = "Krefeld";
+
+// Задание 9.2
+user.age++;
+
+/* 
+//Задание 9.3
+delete user.name;
+*/
+
+// Задание 9.4
+let info = prompt(`Какую информацию хотите узнать о пользователе?`, `name`);
+alert(user[info]);
+
 // Задание 10
-
-
-
+let name = prompt('Введите ваше имя:');
+alert(`Привет, ${name}!`);
